9c60137cbb936d5f75e6aa224a08998ec1154a00
Libo0228 <1079514625@qq.com>
2018-01-24 21:05:22 +0800

    ddd

--- QuarterHour/app/src/main/java/com/example/quarterhour/adapter/MyCehuaAdapter.java
+++ QuarterHour/app/src/main/java/com/example/quarterhour/adapter/MyCehuaAdapter.java
@@ -1,18 +1,18 @@
 package com.example.quarterhour.adapter;
 
 import android.content.Context;
-import android.media.Image;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.quarterhour.R;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.util.List;
-
 
 /**
  * Created by zm on 2018/1/1.
@@ -49,67 +49,66 @@ public class MyCehuaAdapter extends BaseAdapter {
         viewHolder holder;
         viewHolder2 holder2;
         int type = getItemViewType(position);
-        if (convertView == null){
-            holder = new viewHolder();
-            holder2 = new viewHolder2();
-            switch (type){
-                case TYPE_1:
-                    convertView = View.inflate(context, R.layout.cehua_listviewitem1, null);
-                    holder.touxiang =  convertView.findViewById(R.id.image_touxiang);
-                    holder.name =  convertView.findViewById(R.id.cehua_name);
-                    holder.sex =  convertView.findViewById(R.id.cehua_image_xingbie);
-                    break;
 
-                case  TYPE_2:
-                    convertView = View.inflate(context, R.layout.cehua_listviewitem2, null);
-                    holder2.imageView =  convertView.findViewById(R.id.cehua_image_tubiao);
-                    holder2.text_title =  convertView.findViewById(R.id.cehua_text);
-                    break;
+        if (type == TYPE_1) {
+            if (convertView == null) {
+                holder = new viewHolder();
+                convertView = View.inflate(context, R.layout.cehua_listviewitem1, null);
+                holder.relativeLayout = convertView.findViewById(R.id.cehua_relative);
+                holder.touxiang = convertView.findViewById(R.id.image_touxiang);
+                holder.name = convertView.findViewById(R.id.cehua_name);
+                holder.sex = convertView.findViewById(R.id.cehua_image_xingbie);
+                convertView.setTag(holder);
+            } else {
+                holder = (viewHolder) convertView.getTag();
+
             }
-            convertView.setTag(holder);
-            convertView.setTag(holder2);
-        }else {
-//            holder = (viewHolder) convertView.getTag();
-            holder2 = (viewHolder2) convertView.getTag();
-        }
+            if (position == 0) {
+                holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(context, "基本信息", Toast.LENGTH_SHORT).show();
 
-        switch (type) {
-            case TYPE_1:
+                    }
+                });
+            }
+            return convertView;
 
+        } else {
 
+            if (convertView == null) {
+                holder2 = new viewHolder2();
+                convertView = View.inflate(context, R.layout.cehua_listviewitem2, null);
+                holder2.imageView = convertView.findViewById(R.id.cehua_image_tubiao);
+                holder2.text_title = convertView.findViewById(R.id.cehua_text);
+                convertView.setTag(holder2);
+            } else {
+                holder2 = (viewHolder2) convertView.getTag();
 
-                break;
-            case TYPE_2:
+            }
+            if (position == 1) {
+                holder2.imageView.setImageResource(R.mipmap.xin);
+                holder2.text_title.setText("我的关注");
+            }
 
-
-
-                if (position == 1){
-                    holder2.imageView.setImageResource(R.mipmap.xin);
-                    holder2.text_title.setText("我的关注");
-                }
-
-                if (position == 2){
-                    holder2.imageView.setImageResource(R.mipmap.xingxing);
-//                    ImageLoader.getInstance().displayImage(String.valueOf(R.mipmap.shezhi),holder2.imageView);
+            if (position == 2) {
+                holder2.imageView.setImageResource(R.mipmap.xingxing);
                 holder2.text_title.setText("我的收藏");
             }
-                if (position == 3){
-                    holder2.imageView.setImageResource(R.mipmap.menu_sousuo);
-//                    ImageLoader.getInstance().displayImage(String.valueOf(R.mipmap.shezhi),holder2.imageView);
-                    holder2.text_title.setText("搜索好友");
-                }
-                if (position == 4){
-                    holder2.imageView.setImageResource(R.mipmap.lingdang);
-//                    ImageLoader.getInstance().displayImage(String.valueOf(R.mipmap.shezhi),holder2.imageView);
-                    holder2.text_title.setText("消息通知");
+            if (position == 3) {
+                holder2.imageView.setImageResource(R.mipmap.menu_sousuo);
+                holder2.text_title.setText("搜索好友");
+            }
+            if (position == 4) {
+                holder2.imageView.setImageResource(R.mipmap.lingdang);
+                holder2.text_title.setText("消息通知");
 
-                }
+            }
 
-                break;
+            return convertView;
+
         }
-        return convertView;
     }
-
     @Override
     public int getViewTypeCount() {
         return 2;
@@ -125,6 +124,7 @@ public class MyCehuaAdapter extends BaseAdapter {
     }
 
     class viewHolder{
+        RelativeLayout relativeLayout;
         SimpleDraweeView touxiang;
         TextView name;
         ImageView sex;
