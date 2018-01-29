0ba723eb9b1ba82d097888c63274b6e040ae6d4e
wangmingli <wangsong5211@qq.com>
2018-01-29 19:17:58 +0800

    //王名利

--- QuarterHour/app/src/main/java/com/example/quarterhour/appliction/Myapplication.java
+++ QuarterHour/app/src/main/java/com/example/quarterhour/appliction/Myapplication.java
@@ -48,6 +48,12 @@ public class Myapplication extends Application{
         PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
         //QQ
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+
+        PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
+        //新浪微博(第三个参数为回调地址)
+        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad","http://sns.whalecloud.com/sina2/callback");
+        //QQ
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
     }
 
 }
